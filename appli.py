45949ec073a88f3441c62f99633f3969bf6ae2e8
km88469879 <58740031+km88469879@users.noreply.github.com>
2020-07-11 02:34:18 -0700

    Add files via upload

---
+++ appli.py
@@ -0,0 +1,26 @@
+print("Om Shanti")
+chus = input("select your whatever: ")
+list = ["plus", "minus", "multipication"]
+if (list[0])== chus:
+    x = int(input("first number: "))
+    y = int(input("second number: "))
+    z = int(x +y)
+    print(z)
+elif (list[1]) == chus:
+    x = int(input("first number: "))
+    y = int(input("second number: "))
+    z = int(x - y)
+    print(z)
+
+elif (list[2]) == chus:
+    x = int(input("first number: "))
+    y = int(input("second number: "))
+    z = int(x * y)
+    print(z)
+
+else:print("Only (plus, minus, multipication) are valid")
+
+
+
+
+
